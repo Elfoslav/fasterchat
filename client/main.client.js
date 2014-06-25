7a3d0e9a5706f6fb6cbbd37bf2746be71d75a7f1
Tomáš Hromník <tom.hromnik@gmail.com>
2014-06-25 16:26:39 +0200

    Repaired bug when user came not logged in, then logged in and he did not see "writing now" - message stream wrapped with Deps.autorun

--- client/main.client.js
+++ client/main.client.js
@@ -1,29 +1,33 @@
 Meteor.startup(function() {
   window.onbeforeunload = onWindowClose;
 
-  //handler on message typing
-  messageStream.on('msg' + Meteor.userId(), function(msg) {
-    if(msg) {
-      $('.typing-now').removeClass('hidden');
-      $('.typing-now-text').text(msg);
-    } else {
-      $('.typing-now').addClass('hidden');
-    }
-  });
+  Deps.autorun(function() {
 
-  //when user sends a message
-  messageStream.on('msgSent' + Meteor.userId(), function(msgId, senderFbId) {
-    if($('.typing-now').length) {
-      //user is in /chat page
-      $('.typing-now-text').text('');
-      $('.typing-now').addClass('hidden');
-      Meteor.call('markMessageAsRead', msgId);
-    } else {
-      //update unread messages
-      if(typeof Session.get('unreadMessages' + senderFbId) !== 'undefined') {
-        Session.set('unreadMessages' + senderFbId, Session.get('unreadMessages' + senderFbId) + 1);
+    //handler on message typing
+    messageStream.on('msg' + Meteor.userId(), function(msg) {
+      if(msg) {
+        $('.typing-now').removeClass('hidden');
+        $('.typing-now-text').text(msg);
+      } else {
+        $('.typing-now').addClass('hidden');
       }
-    }
+    });
+
+    //when user sends a message
+    messageStream.on('msgSent' + Meteor.userId(), function(msgId, senderFbId) {
+      if($('.typing-now').length) {
+        //user is in /chat page
+        $('.typing-now-text').text('');
+        $('.typing-now').addClass('hidden');
+        Meteor.call('markMessageAsRead', msgId);
+      } else {
+        //update unread messages
+        if(typeof Session.get('unreadMessages' + senderFbId) !== 'undefined') {
+          Session.set('unreadMessages' + senderFbId, Session.get('unreadMessages' + senderFbId) + 1);
+        }
+      }
+    });
+
   });
 });
 
